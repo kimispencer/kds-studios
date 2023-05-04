4289e3edacd5d47534b57bfed7caa0dd4b4a308b
kimispencer <kimispencer@gmail.com>
2023-05-03 22:30:14 -0400

    project detail summary

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -36,14 +36,14 @@ const ProjectDetailPage: NextPage<Props> = ({ project, projects }: Props) => {
           <h3>{project.description}</h3>
         </div>
       </div>
-      <div>
+      <div className={styles.projectDetailImagesContainer}>
         {project.images.map((image, i) => {
           return (
             image.imageType === "desktop" || image.imageType === "iphone"
             ? <div
                 key={project.slug + "-" + i}
                 className={concatClassnames(
-                  styles.projectDetailSlide,
+                  styles.projectDetailImageSlide,
                   image.imageType === "desktop" ? styles.desktop : styles.iphone
                 )}
               >
@@ -57,7 +57,10 @@ const ProjectDetailPage: NextPage<Props> = ({ project, projects }: Props) => {
                   <h4>{image.imageText}</h4>
                 </div>
               </div>
-            : <div key={i} className={concatClassnames(styles.projectDetailSlide, styles.screenshot)}>
+            : <div
+                key={project.slug + "-" + i}
+                className={concatClassnames(styles.projectDetailImageSlide, styles.screenshot)}
+              >
                 <ScreenshotImage
                   image={image}
                   imageKey={project.imageKey}
@@ -68,6 +71,19 @@ const ProjectDetailPage: NextPage<Props> = ({ project, projects }: Props) => {
           )
         })
         }
+      </div>
+      <div className={styles.projectDetailSummarySlide}>
+        <div className={styles.projectDetailSummary}>
+          <div className={styles.projectDetailSummaryDescription}>
+            <p>Dignissim suspendisse in est ante in nibh mauris cursus mattis. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere. Leo a diam sollicitudin tempor id eu nisl. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Adipiscing diam donec adipiscing tristique. Ultrices tincidunt arcu non sodales. Amet purus gravida quis blandit turpis cursus. In hac habitasse platea dictumst. Cras fermentum odio eu feugiat pretium nibh ipsum consequat. Nisi est sit amet facilisis magna etiam tempor. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis.</p>
+            <h4 className={styles.projectDetailLink}>Visit the site</h4>
+          </div>
+          <div className={styles.projectSkills}>
+            <h4>Creative Direction</h4>
+            <h4>Design</h4>
+            <h4>Development</h4>
+          </div>
+        </div>
       </div>
     </div>
     <ProjectNav projects={projects} currentSlug={project.slug} />
