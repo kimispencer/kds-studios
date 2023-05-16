f8357b80af005346a952a106a049fcfea12cae0d
kimispencer <kimispencer@gmail.com>
2023-05-16 12:28:51 -0400

    image src/url function cleanup

--- lib/functions.tsx
+++ lib/functions.tsx
@@ -2,13 +2,13 @@ export function concatClassnames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-export function getImageUrl(imageKey: String, fileName: String) {
-  const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
-  const folderName = "KDS-studios"
-  return `https://res.cloudinary.com/${cloudName}/image/upload/${folderName}/${imageKey}/${fileName}`;
-}
+const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+const folderName = "KDS-studios"
 
 export function getImageSrc(imageKey: String, fileName: String) {
-  const folderName = "KDS-studios"
   return `${folderName}/${imageKey}/${fileName}`;
+}
+
+export function getImageUrl(imageKey: String, fileName: String) {
+  return `https://res.cloudinary.com/${cloudName}/image/upload/${getImageSrc(imageKey, fileName)}`;
 }
