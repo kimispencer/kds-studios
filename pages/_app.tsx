85b5aafefde7824a71f70d71b210da08868a1f54
kimispencer <kimispencer@gmail.com>
2023-05-22 10:30:18 -0400

    meta tags for open graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,9 +11,15 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
     <Head>
       <title>KDS Studios</title>
-      <meta name="description" content="KDS Studio's portfolio site" />
+      <meta name="description" content="KDS Studios" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/kds.ico" />
+      <meta property="og:title"content="KDS Studios"/>
+      <meta property="og:image" content="https://res.cloudinary.com/dq4mmu4m3/image/upload/w_1200,h_620,c_crop/v1684340256/KDS-studios/about/profile_bdcmco.jpg" />
+      <meta property="og:description" content="KDS Studios creates digital identity and experiences through concept development, UI/UX & visual design, and creative coding solutions." />
+      <meta property="og:url" content="kds.studio"/>
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Head>
     <div className={chivo.className}>
       <Layout>
